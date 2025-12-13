ec7f46c30c4597a9fc200344821445a71cf1ce7c
hungchou8994 <tanhungnguyen8994@gmail.com>
2025-12-13 17:06:37 +0000

    optv2

--- scripts/build_cuda.sh
+++ scripts/build_cuda.sh
@@ -47,6 +47,25 @@ nvcc -std=c++17 -O2 \
     main_cuda_opt_v1.o autoencoder_opt_v1.o \
     -o autoencoder_cuda_opt_v1
 
+# Build optimized version 2 (Speed Optimized)
+echo "Building opt_v2 (Speed Optimized)..."
+nvcc -std=c++17 -O3 -use_fast_math -c \
+    "${PROJECT_ROOT}/src/cuda/autoencoder_opt_v2.cu" \
+    -I"${PROJECT_ROOT}/include" \
+    -I"${PROJECT_ROOT}/src/data" \
+    -o autoencoder_opt_v2.o
+
+nvcc -std=c++17 -O3 -c \
+    -DVERSION_NAME='"CUDA OPT_V2 (Speed Optimized)"' \
+    "${PROJECT_ROOT}/src/main_cuda.cpp" \
+    -I"${PROJECT_ROOT}/include" \
+    -I"${PROJECT_ROOT}/src/data" \
+    -o main_cuda_opt_v2.o
+
+nvcc -std=c++17 -O3 -use_fast_math \
+    main_cuda_opt_v2.o autoencoder_opt_v2.o \
+    -o autoencoder_cuda_opt_v2
+
 # Skip optimized version 2 for now (not implemented)
 # # Build optimized version 2     -o main_cuda_opt_v1.o
 # 
